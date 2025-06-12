8e38aeb007f62eac446158a1767e1f7c7d8b17e1
Payal Saini <sainipihu1235@gmail.com>
2025-06-12 22:56:36 +0530

    Update MapComponent.jsx

--- Frontend/realEstate-react-project/src/pages/components/MapComponent.jsx
+++ Frontend/realEstate-react-project/src/pages/components/MapComponent.jsx
@@ -2,13 +2,14 @@ import React, { useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-//  Fix Leaflet marker image paths for Netlify/Vite
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
 });
