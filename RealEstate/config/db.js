af61f98b2ca43defd11ced5cd8155fe1b50410e5
Payal Saini <sainipihu1235@gmail.com>
2025-07-31 21:00:52 +0530

    Update db.js

--- RealEstate/config/db.js
+++ RealEstate/config/db.js
@@ -5,14 +5,12 @@ require("dotenv").config(); // Load environment variables
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI)
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
+ 
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1); 
   }
 };
 
