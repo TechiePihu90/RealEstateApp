a6eb4d722aad8b005749d6ce7c2654dc5b120b0b
Payal Saini <sainipihu1235@gmail.com>
2025-09-19 22:44:41 +0530

    Update authMiddleware.js

--- RealEstate/middleware/authMiddleware.js
+++ RealEstate/middleware/authMiddleware.js
@@ -3,20 +3,24 @@ const User = require("../models/userModel");
 
 const protect = async (req, res, next) => {
   let token;
-
+   
   if (!process.env.JWT_SECRET) {
     console.error("JWT_SECRET is not defined in environment variables");
     return res.status(500).json({ message: "Internal Server Error" });
   }
 
+  //checks if the user has authorization header like Authorization: Bearer <token> if yes extract token
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     try {
       token = req.headers.authorization.split(" ")[1];
       console.log("Extracted Token:", token);
 
+      //verify token using jwt.verify if token is valid you will get payload user_id 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       console.log("Decoded Token:", decoded);
 
+      //Use the userId from token to find the actual user in the database.
+//Attach that user object to req.user so other routes can access it .select("-password") → don’t include the password field
       req.user = await User.findById(decoded.userId).select("-password");
 
       if (!req.user) {
@@ -25,7 +29,7 @@ const protect = async (req, res, next) => {
       }
 
       console.log("Authenticated User:", req.user);
-      next(); // ✅ Proceed to next middleware
+      next(); 
 
     } catch (error) {
       console.error("JWT Verification Error:", error.message);
