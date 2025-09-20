cb497bec1913eba7bf438cf3a9465ab13b9fdb4d
Payal Saini <sainipihu1235@gmail.com>
2025-09-20 10:07:32 +0530

    Update propertyRoute.js

--- RealEstate/routes/propertyRoute.js
+++ RealEstate/routes/propertyRoute.js
@@ -7,20 +7,20 @@ const {
   deleteProperty
 } = require("../controllers/propertyController");
 const { protect } = require("../middleware/authMiddleware");
-const upload = require("../middleware/uploadMiddleware"); // ✅ Import multer middleware
+const upload = require("../middleware/uploadMiddleware"); // Import multer middleware
 
 const router = express.Router();
 
-// ✅ Search route pehle rakho taaki conflict na ho
+//  Search route pehle rakha taaki conflict na ho
 router.get("/search/:location", searchProperties);
 
-// ✅ Upload images while creating property (with authentication)
+// Upload images while creating property (with authentication)
 router.post("/", protect, upload.array("images", 5), createProperty);
 
-// ✅ Get all properties
+// Get all properties
 router.get("/", getProperties);
 
-// ✅ Get a property by ID
+// Get a property by ID
 router.get("/:id", getPropertyById);
 
 router.delete("/:id", protect, deleteProperty);
