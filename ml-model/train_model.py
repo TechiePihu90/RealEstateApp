16f2a29575557f7814dfe45b3b98aad16eb90585
Payal Saini <sainipihu1235@gmail.com>
2025-09-22 21:51:47 +0530

    Update train_model.py

--- ml-model/train_model.py
+++ ml-model/train_model.py
@@ -51,4 +51,4 @@ model.fit(X_train, y_train)
 with open("bangalore_price_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
-print("✅ Model trained and saved successfully!")
+print("Model trained and saved successfully!")
